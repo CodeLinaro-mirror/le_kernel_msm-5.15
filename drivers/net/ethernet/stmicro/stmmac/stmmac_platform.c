sirdarckcat/ubuntu

soc: net: stmmac: qed: Add support for Mac2Mac
This commit adds support for mac2mac connectivity. This
functionality needs to be enabled via a dts entry.

Change-Id: Icc3bccd240b360973587036792f622da577de783

drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -440,6 +440,9 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 	/* Default to phy auto-detection */
 	plat->phy_addr = -1;
 
+	/* Flag for mac2mac feature support*/
+	plat->mac2mac_en = of_property_read_bool(np, "mac2mac");
+
 	/* Default to get clk_csr from stmmac_clk_crs_set(),
 	 * or get clk_csr from device tree.
 	 */
@@ -453,9 +456,11 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 		dev_warn(&pdev->dev, "snps,phy-addr property is deprecated\n");
 
 	/* To Configure PHY by using all device-tree supported properties */
-	rc = stmmac_dt_phy(plat, np, &pdev->dev);
-	if (rc)
-		return ERR_PTR(rc);
+	if (!plat->mac2mac_en) {
+		rc = stmmac_dt_phy(plat, np, &pdev->dev);
+		if (rc)
+			return ERR_PTR(rc);
+	}
 
 	of_property_read_u32(np, "tx-fifo-depth", &plat->tx_fifo_size);
 

CodeLinaro-mirror/le_kernel_msm-5.15

drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -466,6 +466,9 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 	/* Default to phy auto-detection */
 	plat->phy_addr = -1;
 
+	/* Flag for mac2mac feature support*/
+	plat->mac2mac_en = of_property_read_bool(np, "mac2mac");
+
 	/* Default to get clk_csr from stmmac_clk_crs_set(),
 	 * or get clk_csr from device tree.
 	 */
@@ -478,10 +481,12 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 	if (of_property_read_u32(np, "snps,phy-addr", &plat->phy_addr) == 0)
 		dev_warn(&pdev->dev, "snps,phy-addr property is deprecated\n");
 
-	rc = stmmac_mdio_setup(plat, np, &pdev->dev);
-	if (rc) {
-		ret = ERR_PTR(rc);
-		goto error_put_phy;
+	if (!plat->mac2mac_en) {
+		rc = stmmac_mdio_setup(plat, np, &pdev->dev);
+		if (rc) {
+			ret = ERR_PTR(rc);
+			goto error_put_phy;
+		}
 	}
 
 	of_property_read_u32(np, "tx-fifo-depth", &plat->tx_fifo_size);